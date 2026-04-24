aa8daa7cab32dfca361ade2c95e04dc0d6164894
Maksud Nurullaev <maksud@elixir4abs>
2026-04-24 10:57:01 +0500

    Fix excludes for formater

--- .formatter.exs
+++ .formatter.exs
@@ -2,6 +2,11 @@
   import_deps: [:phoenix, :phoenix_live_view],
   subdirectories: ["priv/*/migrations"],
   plugins: [Phoenix.LiveView.HTMLFormatter],
+  excludes: [
+    "lib/elixir4abs_validators_web/live/swift_pacs008_live.ex",
+    "lib/elixir4abs_validators_web/live/qr_payment_live.ex",
+    "lib/elixir4abs_validators_web/live/rules_viewer_live.ex"
+  ],
   inputs: [
     "*.{ex,exs}",
     "{config,test}/**/*.{ex,exs}",
