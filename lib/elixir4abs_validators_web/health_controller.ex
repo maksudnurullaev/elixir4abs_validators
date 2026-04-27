f5e2d91aa2b0e613813db1e7935fa0c5fe02e7c9
Maksud Nurullaev <maksud@elixir4abs>
2026-04-27 14:12:53 +0500

    Just for test

--- lib/elixir4abs_validators_web/health_controller.ex
+++ lib/elixir4abs_validators_web/health_controller.ex
@@ -2,6 +2,6 @@ defmodule Elixir4absValidatorsWeb.HealthController do
   use Phoenix.Controller, formats: [:json]
 
   def check(conn, _params) do
-    json(conn, %{status: "ok"})
+    json(conn, %{status: "ok-v2"})
   end
 end
