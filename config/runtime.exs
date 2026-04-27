5b4c727788759ce160814728061c78abc1b55339
Maksud Nurullaev <maksud@elixir4abs>
2026-04-27 15:51:25 +0500

    Fix longpull error

--- config/runtime.exs
+++ config/runtime.exs
@@ -16,5 +16,6 @@ if config_env() == :prod do
   config :elixir4abs_validators, Elixir4absValidatorsWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
     http: [ip: {0, 0, 0, 0, 0, 0, 0, 0}, port: port],
-    secret_key_base: secret_key_base
+    secret_key_base: secret_key_base,
+    check_origin: ["https://#{host}", "https://www.#{host}"]
 end
